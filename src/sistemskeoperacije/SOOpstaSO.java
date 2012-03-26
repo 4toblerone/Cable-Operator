f224a7c327e053ddc1dc9b06eb7f970e995e7bae
Sasa Trifunovic <sasa.s.trifunovic@gmail.com>
2012-03-26 14:52:35 +0200

    Making code more readable

--- src/sistemskeoperacije/SOOpstaSO.java
+++ src/sistemskeoperacije/SOOpstaSO.java
@@ -12,75 +12,77 @@ import kab.op.domen.DomenskiObjekat;
  * @author Sale
  */
 public abstract class SOOpstaSO {
-    
-    
+
     static int signal;
-    
-    public int  izvrsiSO(DomenskiObjekat obj) {
+
+    public int izvrsiSO(DomenskiObjekat obj) {
         signal = 0;
-        
-       if( !izvrsiPreduslov(obj)){ 
-           System.out.println(signal);
-           return signal;}
+
+        if (!izvrsiPreduslov(obj)) {
+            System.out.println(signal);
+            return signal;
+        }
         izvrsiOperaciju(obj);
         potvrdiIzvrsenjeSO();
         System.err.println(signal);
         return signal;
     }
 
-   protected abstract boolean izvrsiPreduslov(DomenskiObjekat obj) ;
+    protected abstract boolean izvrsiPreduslov(DomenskiObjekat obj);
 
-   //ovde ce se setovati signal koji ce kasnije proveravati
-   //u potvrdi izvrsenje SO
-    protected abstract int izvrsiOperaciju(DomenskiObjekat obj) ;
+    //ovde ce se setovati signal koji ce kasnije proveravati
+    //u potvrdi izvrsenje SO
+    protected abstract int izvrsiOperaciju(DomenskiObjekat obj);
 
     /*protected boolean ponistiIzvrsenjeSO() {
-       return true;
+    return true;
     }*/
-
     protected boolean potvrdiIzvrsenjeSO() {
-        int signal1 ;
-        if(stanjeOperacije(signal)){
-        signal1 = KomunikacijaSaBazom.vratiInstancuKomunikacijeSaBazom().potvrdiTransakciju();
-        
-        if(!stanjeOperacijeOpstaSO(signal1)){
-                signal = signal1;  
+        int signal1;
+        if (stanjeOperacije(signal)) {
+            signal1 = KomunikacijaSaBazom.vratiInstancuKomunikacijeSaBazom().potvrdiTransakciju();
+
+            if (!stanjeOperacijeOpstaSO(signal1)) {
+                signal = signal1;
+                return false;
+            }
+        } else {
+            signal1 = KomunikacijaSaBazom.vratiInstancuKomunikacijeSaBazom().ponistiTransakciju();
+
+            if (!stanjeOperacijeOpstaSO(signal1)) {
+                signal = signal1;
                 return false;
             }
         }
-        else{
-            signal1= KomunikacijaSaBazom.vratiInstancuKomunikacijeSaBazom().ponistiTransakciju();
-            
-            if(!stanjeOperacijeOpstaSO(signal1)){
-                signal = signal1;
-                return false;
-            }    
-        }
         return true;
     }
-    
-  abstract boolean stanjeOperacije(int signal);
-    
-    boolean stanjeOperacijeOpstaSO(int signal){
-        
-        switch(signal){
-            case 51 : return true;
-            case 52 :  return false;
-            case 53 : return true;
-            case 54 : return false;
+
+    abstract boolean stanjeOperacije(int signal);
+
+    boolean stanjeOperacijeOpstaSO(int signal) {
+
+        switch (signal) {
+            case 51:
+                return true;
+            case 52:
+                return false;
+            case 53:
+                return true;
+            case 54:
+                return false;
         }
         return false;
-                
+
     }
-    
+
     //implementacijom singlotona to je vec odradjeno
-    private boolean otvoriBazu(){
-     
+    private boolean otvoriBazu() {
+
         return true;
     }
-    
+
     //da li je neophodno?
-    private boolean zatvoriBazu(){
+    private boolean zatvoriBazu() {
         return true;
     }
 }
